51c480f4df1d86854eb32ff4d53f1a041dab5033
tymon42 <tymon42@outlook.com>
2022-04-08 15:09:38 +0800

    重构

---
+++ main.py
@@ -0,0 +1,25 @@
+import imp
+import threading
+import queue
+import spider
+# import time
+
+
+
+def consumer(queue: queue.Queue):
+    while True:
+        gift = giftRecived.get()
+        print(gift)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    giftRecived = queue.Queue()
+
+    t1 = threading.Thread(target=spider.KeepEyeOn, args=(7777, giftRecived, ))
+    t2 = threading.Thread(target=consumer, args=(giftRecived, ))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    #TODO: 销毁线程
---
+++ spider.py
@@ -0,0 +1,49 @@
+import BiliLive.blivedm as blivedm
+import queue
+import asyncio
+
+# taskType=[str,queue.Queue]
+
+def KeepEyeOn(room_id:str, queue:queue.Queue):
+    loop = asyncio.new_event_loop()
+    task = [run_single_client(room_id, queue)]
+    loop.run_until_complete(asyncio.wait(task))
+    loop.close()
+
+async def run_single_client(room_id, queue):
+    """
+    监听一个直播间
+    """
+    # 如果SSL验证失败就把ssl设为False，B站真的有过忘续证书的情况
+    client = blivedm.BLiveClient(room_id, ssl=True)
+    handler = MyHandler(queue)
+    client.add_handler(handler)
+
+    client.start()
+    try:
+        await client.join()
+    finally:
+        await client.stop_and_close()
+
+
+class MyHandler(blivedm.BaseHandler):
+
+    def __init__(self, queue: queue.Queue):
+        self.queue = queue
+
+    async def _on_gift(self, client: blivedm.BLiveClient, message: blivedm.GiftMessage):
+        # print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
+        #       f' （{message.coin_type}瓜子x{message.total_coin}）')
+        # if message.coin_type != 'silver':
+        self.queue.put([message.uid, message.price])
+        # print(self.queue.qsize())
+
+    async def _on_buy_guard(self, client: blivedm.BLiveClient, message: blivedm.GuardBuyMessage):
+        # print(f'[{client.room_id}] {message.username} 购买{message.gift_name}')
+        self.queue.put([message.uid, message.price])
+        # print(self.queue.qsize())
+
+    async def _on_super_chat(self, client: blivedm.BLiveClient, message: blivedm.SuperChatMessage):
+        # print(f'[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
+        self.queue.put([message.uid, message.price])
+        # print(self.queue.qsize())
