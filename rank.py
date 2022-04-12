65dccba087c5f62cd821a0a02e3217954a5737b4
Cunoe <cunkycheng@gmail.com>
2022-04-12 12:06:13 +0800

    重构rank.py中的排序函数

--- rank.py
+++ rank.py
@@ -16,6 +16,7 @@ class modelRank(Base):
     # 表结构
     id = Column(Integer, primary_key=True)
     uid = Column(String(100))
+    room_id = Column(Integer, default=0)
     rank = Column(Integer, default=0)
     qn = Column(Integer, default=0)
     ts = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'), server_onupdate=text('CURRENT_TIMESTAMP'))
@@ -35,24 +36,24 @@ class Rank:
         self._Session = sessionmaker(bind=engine)
         self._session = self._Session()
 
-    # 排名主要方法，请传入参数uid以及增加的qn值（注意不是增加后的qn值
-    def rank(self, uid: str, qn: int):
-        search = self._session.query(modelRank).filter(modelRank.uid == uid).first()
+    # 排名主要方法，请传入参数直播间room_id、用户的uid以及增加的qn值（注意不是增加后的qn值
+    def rank(self, room_id: int, uid: str, qn: int):
+        search = self._session.query(modelRank).filter(modelRank.uid == uid and modelRank.room_id == room_id).first()
         if search:
             if search.qn != qn:
-                self.update_func(uid, qn)
+                self.update_func(room_id, uid, qn)
         else:
-            self.create_func(uid, qn)
+            self.create_func(room_id, uid, qn)
 
-    def update_func(self, uid: str, qn: int):
-        self._qn = self._session.query(modelRank).filter(modelRank.uid == uid).first().qn
+    def update_func(self, room_id: int, uid: str, qn: int):
+        self._qn = self._session.query(modelRank).filter(modelRank.uid == uid and modelRank.room_id == room_id).first().qn
         self._session.query(modelRank).filter(modelRank.uid == uid).delete()
         self._session.commit()
-        self.create_func(uid, qn=qn + self._qn)  # 将原qn值添加后传入creat_func处理
+        self.create_func(room_id, uid, qn=qn + self._qn)  # 将原qn值添加后传入creat_func处理
 
-    def create_func(self, uid: str, qn: int):
+    def create_func(self, room_id: int, uid: str, qn: int):
         self._rank = 0
-        search = self._session.query(modelRank).order_by(modelRank.rank.desc()).all()
+        search = self._session.query(modelRank).order_by(modelRank.rank.desc() and modelRank.room_id == room_id).all()
         if search:
             if qn > search[0].qn:
                 self._rank = search[0].rank + self.add
@@ -68,9 +69,9 @@ class Rank:
                     if search[i].qn >= qn >= search[i + 1].qn:
                         self._rank = (search[i].rank + search[i + 1].rank) / 2
                         break
-            self._session.add(modelRank(uid=uid, rank=int(self._rank), qn=qn))
+            self._session.add(modelRank(room_id=room_id, uid=uid, rank=int(self._rank), qn=qn))
             self._session.commit()
         else:
             self._rank = (max - min) / 2
-            self._session.add(modelRank(uid=uid, rank=self._rank, qn=qn))
+            self._session.add(modelRank(room_id=room_id, uid=uid, rank=self._rank, qn=qn))
             self._session.commit()
