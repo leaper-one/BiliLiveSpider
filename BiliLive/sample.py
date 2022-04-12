0bc270275fdc1dfad83a121ece5c1bab0f9f4359
Cunoe <cunkycheng@gmail.com>
2022-04-12 11:55:47 +0800

    重构rank.py中的排序函数

--- BiliLive/sample.py
+++ BiliLive/sample.py
@@ -1,66 +1,49 @@
-# -*- coding: utf-8 -*-
-import asyncio
-import random
+import queue
 
 import blivedm
 
+# giftQueue = queue.Queue()
 # 直播间ID的取值看直播间URL
 TEST_ROOM_IDS = [
-    12235923,
-    14327465,
-    21396545,
-    21449083,
-    23105590,
+    7777,
 ]
+# giftRecived = Queue()
+
+# class consumer(Thread):
+#     def __init__(self, queue):
+#         Thread.__init__(self)
+#         self.queue = queue
+#
+#         def run(self):
+#             while True:
+#                 gift = self.queue.get()
+#                 print(gift)
+#                 self.queue.take_done()
+
+async def main(room, queue):
+    await run_single_client(room, queue)
 
 
-async def main():
-    await run_single_client()
-    await run_multi_client()
-
-
-async def run_single_client():
+async def run_single_client(room, queue):
     """
     演示监听一个直播间
     """
-    room_id = random.choice(TEST_ROOM_IDS)
+    room_id = room
     # 如果SSL验证失败就把ssl设为False，B站真的有过忘续证书的情况
     client = blivedm.BLiveClient(room_id, ssl=True)
-    handler = MyHandler()
+    handler = MyHandler(queue)
     client.add_handler(handler)
 
     client.start()
     try:
-        # 演示5秒后停止
-        await asyncio.sleep(5)
-        client.stop()
-
         await client.join()
     finally:
         await client.stop_and_close()
 
-
-async def run_multi_client():
-    """
-    演示同时监听多个直播间
-    """
-    clients = [blivedm.BLiveClient(room_id) for room_id in TEST_ROOM_IDS]
-    handler = MyHandler()
-    for client in clients:
-        client.add_handler(handler)
-        client.start()
-
-    try:
-        await asyncio.gather(*(
-            client.join() for client in clients
-        ))
-    finally:
-        await asyncio.gather(*(
-            client.stop_and_close() for client in clients
-        ))
-
-
 class MyHandler(blivedm.BaseHandler):
+
+    def __init__(self, queue: queue.Queue):
+        self.queue = queue
     # # 演示如何添加自定义回调
     # _CMD_CALLBACK_DICT = blivedm.BaseHandler._CMD_CALLBACK_DICT.copy()
     #
@@ -69,23 +52,26 @@ class MyHandler(blivedm.BaseHandler):
     #     print(f"[{client.room_id}] INTERACT_WORD: self_type={type(self).__name__}, room_id={client.room_id},"
     #           f" uname={command['data']['uname']}")
     # _CMD_CALLBACK_DICT['INTERACT_WORD'] = __interact_word_callback  # noqa
-
-    async def _on_heartbeat(self, client: blivedm.BLiveClient, message: blivedm.HeartbeatMessage):
-        print(f'[{client.room_id}] 当前人气值：{message.popularity}')
-
-    async def _on_danmaku(self, client: blivedm.BLiveClient, message: blivedm.DanmakuMessage):
-        print(f'[{client.room_id}] {message.uname}：{message.msg}')
+    #
+    # async def _on_heartbeat(self, client: blivedm.BLiveClient, message: blivedm.HeartbeatMessage):
+    #     print(f'[{client.room_id}] 当前人气值：{message.popularity}')
+    #
+    # async def _on_danmaku(self, client: blivedm.BLiveClient, message: blivedm.DanmakuMessage):
+    #     print(f'[{client.room_id}] {message.uname}：{message.msg}')
 
     async def _on_gift(self, client: blivedm.BLiveClient, message: blivedm.GiftMessage):
-        print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
-              f' （{message.coin_type}瓜子x{message.total_coin}）')
+        # print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
+        #       f' （{message.coin_type}瓜子x{message.total_coin}）')
+        # if message.coin_type != 'silver':
+        self.queue.put([message.uid, message.price])
+        print(self.queue.qsize())
 
     async def _on_buy_guard(self, client: blivedm.BLiveClient, message: blivedm.GuardBuyMessage):
-        print(f'[{client.room_id}] {message.username} 购买{message.gift_name}')
+        # print(f'[{client.room_id}] {message.username} 购买{message.gift_name}')
+        self.queue.put([message.uid, message.price])
+        print(self.queue.qsize())
 
     async def _on_super_chat(self, client: blivedm.BLiveClient, message: blivedm.SuperChatMessage):
-        print(f'[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
-
-
-if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+        # print(f'[{client.room_id}] 醒目留言 ¥{message.price} {message.uname}：{message.message}')
+        self.queue.put([message.uid, message.price])
+        print(self.queue.qsize())
---
+++ rank.py
@@ -0,0 +1,76 @@
+from sqlalchemy import create_engine, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, Integer, Text, TIMESTAMP, text
+from sqlalchemy.orm import sessionmaker, relationship
+import time
+
+engine = create_engine(r'sqlite:///db.sqlite3')
+
+Base = declarative_base()
+
+
+# 定义Rank表
+class modelRank(Base):
+    __tablename__ = 'model_rank'
+    # 表结构
+    id = Column(Integer, primary_key=True)
+    uid = Column(String(100))
+    rank = Column(Integer, default=0)
+    qn = Column(Integer, default=0)
+    ts = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'), server_onupdate=text('CURRENT_TIMESTAMP'))
+
+
+class Rank:
+
+    def __init__(self):
+        # 定义Rank表中rank字段的范围
+        self.max = int(10E8)
+        self.min = int(0)
+        self.add = int(10E6)
+
+        self._qn = None
+        self._rank = None
+        # 连接数据库
+        self._Session = sessionmaker(bind=engine)
+        self._session = self._Session()
+
+    # 排名主要方法，请传入参数uid以及增加的qn值（注意不是增加后的qn值
+    def rank(self, uid: str, qn: int):
+        search = self._session.query(modelRank).filter(modelRank.uid == uid).first()
+        if search:
+            if search.qn != qn:
+                self.update_func(uid, qn)
+        else:
+            self.create_func(uid, qn)
+
+    def update_func(self, uid: str, qn: int):
+        self._qn = self._session.query(modelRank).filter(modelRank.uid == uid).first().qn
+        self._session.query(modelRank).filter(modelRank.uid == uid).delete()
+        self._session.commit()
+        self.create_func(uid, qn=qn + self._qn)  # 将原qn值添加后传入creat_func处理
+
+    def create_func(self, uid: str, qn: int):
+        self._rank = 0
+        search = self._session.query(modelRank).order_by(modelRank.rank.desc()).all()
+        if search:
+            if qn > search[0].qn:
+                self._rank = search[0].rank + self.add
+            elif qn < search[-1].qn:
+                self._rank = search[-1].rank - self.add
+            elif len(search) == 1:
+                if qn >= search[0].qn:
+                    self._rank = search[0].rank + self.add
+                else:
+                    self._rank = search[0].rank - self.add
+            else:
+                for i in range(len(search)):
+                    if search[i].qn >= qn >= search[i + 1].qn:
+                        self._rank = (search[i].rank + search[i + 1].rank) / 2
+                        break
+            self._session.add(modelRank(uid=uid, rank=int(self._rank), qn=qn))
+            self._session.commit()
+        else:
+            self._rank = (max - min) / 2
+            self._session.add(modelRank(uid=uid, rank=self._rank, qn=qn))
+            self._session.commit()
